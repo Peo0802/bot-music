8931c87a9a68d188fddec9919e32ffee72c86395
Peo0802 <whatthefack000@gmail.com>
2025-01-03 17:17:52 +0700

    Update owner.js

--- utilityCommands/owner.js
+++ utilityCommands/owner.js
@@ -4,12 +4,12 @@ module.exports = {
     name: 'owner',
     description: 'Bot owner info',
     execute(message, args) {
-        const youtubeLink = '';
-        const InstagramLink = '';
+        const youtubeLink = 'https://www.youtube.com/@GlaceYT';
+        const InstagramLink = 'https://www.instagram.com/glaceytt';
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
-            .setTitle(' 👑 QUAN TRIỀU ĐÌNH 👑')
-            .setDescription(`__**About me**__:\n 🤖 Chúc các quan nghe nhạc vuui vẻ\n ❤️ 💙 )
+            .setTitle(' 🫅 Owner Info')
+            .setDescription(`__**About me**__:\n 🤖 Myself Shiva aka GlaceYT. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n ❤️ [GlceYT](${youtubeLink})\n 💙 [GlaceYTT](${InstagramLink})`)
             .setTimestamp();
 
 
